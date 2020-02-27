7812038f9052052811f20dfb42014ecdc119124c
tommy colella <tcolella@bsd220.org>
2020-02-27 11:30:42 -0600

    Located nearby pizza places

--- Za' Hunter/ViewController.swift
+++ Za' Hunter/ViewController.swift
@@ -10,13 +10,27 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     var region = MKCoordinateRegion()
     let locationManager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        func mapViewDidFinishLoadingMap()
+        {
+            let request = MKLocalSearch.Request()
+                request.naturalLanguageQuery = "pizza"
+                request.region = region
+                let search = MKLocalSearch(request: request)
+                search.start { (response, error) in
+                   if let response = response {
+                      for mapItem in response.mapItems {
+                         print(mapItem.name!)
+                      }
+                   }
+                }
+        }
         
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
@@ -26,17 +40,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    
+        
         let location = locations.first!
         let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
         region = MKCoordinateRegion(center: center, span: span)
         mapView.setRegion(region, animated: true)
     }
-
-    
-    
-    
 }
 
 
