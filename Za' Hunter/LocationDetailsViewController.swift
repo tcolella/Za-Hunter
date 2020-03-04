477a7449369af15e3983a36136d0ab07cb4e0f53
tommy colella <tcolella@bsd220.org>
2020-03-04 10:43:36 -0600

    Added segue to LocationDetailsViewController

---
+++ Za' Hunter/LocationDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailsViewController.swift
+//  Za' Hunter
+//
+//  Created by Tommy Colella on 3/4/20.
+//  Copyright © 2020 Tommy Colella. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Za' Hunter/ViewController.swift
+++ Za' Hunter/ViewController.swift
@@ -11,42 +11,62 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     var region = MKCoordinateRegion()
     let locationManager = CLLocationManager()
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        func mapViewDidFinishLoadingMap()
-        {
-            let request = MKLocalSearch.Request()
-                request.naturalLanguageQuery = "pizza"
-                request.region = region
-                let search = MKLocalSearch(request: request)
-                search.start { (response, error) in
-                   if let response = response {
-                      for mapItem in response.mapItems {
-                         print(mapItem.name!)
-                      }
-                   }
-                }
-        }
         
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
-        
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
+        mapView.delegate = self
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         let location = locations.first!
         let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+        let span = MKCoordinateSpan(latitudeDelta: 0.050, longitudeDelta: 0.050)
         region = MKCoordinateRegion(center: center, span: span)
         mapView.setRegion(region, animated: true)
     }
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "pizza"
+        request.region = region
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    
+                }
+            }
+        }
+    }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+   func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+           performSegue(withIdentifier: "ShowLocationDetailsSegue", sender: nil)
+    }
 }
-
-
